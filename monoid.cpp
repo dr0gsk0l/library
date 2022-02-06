ffd3de90abb1e22bab2ba25dd84e945f0335dea8
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2022-02-06 17:39:29 +0900

    monoid.cpp

---
+++ monoid.cpp
@@ -0,0 +1,19 @@
+template<class T,T (*op)(T, T)>
+struct AsMonoid {
+  std::optional<T> v;
+  AsMonoid():v(){}
+  AsMonoid(T x):v(std::move(x)){}
+  friend AsMonoid operator+(AsMonoid l, AsMonoid r){
+    if(!l)return r;
+    if(!r)return l;
+    return AsMonoid(op(*l.v,*r.v));
+  }
+};
+
+/*
+使用例1
+static constexpr auto f=[](int l, int r){
+  return std::min(l, r);
+};
+using MinInt = AsMonoid<int, f>;
+*/
