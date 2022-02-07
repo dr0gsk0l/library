2b7093613bcda23fdcca6c1fc814fdf261fe532c
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2022-02-07 18:03:58 +0900

    unionfind.cpp

---
+++ unionfind.cpp
@@ -0,0 +1,32 @@
+struct UnionFind{
+  int n;
+  vector<int> r,p;
+  UnionFind(){}
+  UnionFind(int n):num(n),r(n,1),p(n,0){iota(p.begin(),p.end(),0);}
+  
+  int find(int x){
+    return (x==p[x]?x:p[x]=find(p[x]));
+  }
+  
+  bool same(int x,int y){
+    return find(x)==find(y);
+  }
+  
+  bool unite(int x,int y){
+    x=find(x);y=find(y);
+    if(x==y)return false;
+    if(r[x]<r[y])swap(x,y);
+    r[x]+=r[y];
+    p[y]=x;
+    num--;
+    return true;
+  }
+  
+  int size(const int x){
+    return r[find(x)];
+  }
+  
+  int count() const{
+    return num;
+  }
+};
