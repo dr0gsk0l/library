36223974dd1c7cd64158ce99c17de308a56ef4d3
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2022-02-07 22:25:20 +0900

    dijkstra

---
+++ graph/WeightedGraph.cpp
@@ -0,0 +1,18 @@
+template<typename T>
+struct Edge{
+  int to;
+  T cost;
+};
+ 
+template<typename T>
+struct WeightedGraph{
+  int n;
+  vector<vector<Edge<T>>> g;
+    
+  WeightedGraph(){}
+  WeightedGraph(int n):n(n),g(n){}
+    
+  void add_edge(int from, int to, T cost){
+    g[from].emplace_back(to,cost);
+  }
+};
---
+++ graph/dijkstra.cpp
@@ -0,0 +1,22 @@
+template<typename T,typename Comp>
+using pque=priority_queue<T,vector<T>,Comp>;
+
+template<typename T>
+bool chmin(T &a,T b){
+  return (a>b&&(a=b,true));
+}
+
+template<typename T>
+vector<T> dijkstra(const WeightedGraph<T> &g,int s){
+  vector<T> d(n,numeric_limits<T>::max()/2);
+  pque<pair<T,unsigned int>,greater<>> que;
+  d[s]=0;
+  que.emplace(0,s);
+  while(que.size()){
+    auto [now,id]=que.top();que.pop();
+    if(d[id]<now)continue;
+    for(auto&[to,co]:g[id])
+      if(chmin(d[to],now+co))que.emplace(d[to],to);
+  }
+  return d;
+}
