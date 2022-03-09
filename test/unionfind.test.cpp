d7f23cb7e71ae183658cca785294ee8d901446ea
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2022-03-09 13:51:19 +0900

    unionfindtest

---
+++ test/unionfind.test.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../unionfind.cpp"
+
+
+int main() {
+  int n,q;cin>>n>>q;
+  UnionFind uf(n);
+  while(q--){
+    int t,u,v;cin>>t>>u>>v;
+    if(t)cout<<uf.same(u,v)<<"\n";
+    else uf.merge(u,v);
+  }
+}
