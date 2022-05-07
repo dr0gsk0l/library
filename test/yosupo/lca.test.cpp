4dd00afa4e2a79c43e212065e04723b08896c94a
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2022-05-07 18:50:42 +0900

    lcatest

---
+++ test/yosupo/lca.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/lca"
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../../tree/tree.cpp"
+#include "../../tree/lca.cpp"
+
+int main(){
+  int n,q;cin>>n>>q;
+  Tree t(n);
+  for(int i=1;i<n;i++){
+    int p;cin>>p;
+    t.add_edge(i,p);
+  }
+  LCA lca(t,0);
+  while(q--){
+    int u,v;cin>>u>>v;
+    cout<<lca.lca(u,v)<<"\n";
+  }
+}
