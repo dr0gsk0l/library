971bb2a3318ff98ef81e70468d2c412acebb0391
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2022-09-06 17:50:02 +0900

    modify dsu space

--- datastructure/dsu.cpp
+++ datastructure/dsu.cpp
@@ -5,17 +5,17 @@ struct dsu{
   dsu(int n):n(n),num(n),sz(n,1),parent(n,0){iota(parent.begin(),parent.end(),0);}
   
   int leader(int x){ 
-    assert( 0<=x and x<n );
+    assert(0<=x and x<n);
     return (x==parent[x]? x : parent[x]=leader(parent[x])); 
   }
   
   bool same(int x,int y){
-    assert( 0<=x and x<n and 0<=y and y<n);
+    assert(0<=x and x<n and 0<=y and y<n);
     return leader(x)==leader(y); 
   }
   
   bool merge(int x,int y){
-    assert( 0<=x and x<n and 0<=y and y<n);
+    assert(0<=x and x<n and 0<=y and y<n);
     x=leader(x);y=leader(y);
     if(x==y)return false;
     if(sz[x]<sz[y])swap(x,y);
@@ -26,7 +26,7 @@ struct dsu{
   }
   
   int size(const int x){
-    assert( 0<=x and x<n);
+    assert(0<=x and x<n);
     return sz[leader(x)];
   }
   
