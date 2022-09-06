e5716f86a7ae4de10e0609469180e827c0d34038
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2022-09-06 17:15:40 +0900

    mod

---
+++ mod/mint_utility.cpp
@@ -0,0 +1,40 @@
+template<typename MINT>
+class mint_utility {
+  vector<MINT> fact_={MINT::raw(1)};
+  vector<MINT> inv_fact_{MINT::raw(1)};
+  int S=1;//今のサイズ
+ 
+  void extend(const int n){
+    if(n<S)return;
+    const int preS=S;
+    do{S<<=1}while(S<=n);
+
+    fact_.resize(S);
+    for(int i=preS;i<S;i++)fact_[i]=fact_[i-1]*MINT::raw(i);
+
+    inv_fact_.resize(S);
+    inv_fact_.back()=fact_.back().inv();
+    for(int i=S-1;i>preS;i--)inv_fact_[i-1]=inv_fact_[i]*MINT::raw(i);
+  }
+ 
+public:
+  mint_utility() = delete;
+ 
+  static MINT fact(const int n){
+    assert(n>=0);
+    extend(n);
+    return fact_[n];
+  }
+ 
+  static MINT inv_fact(const int n){
+    assert(n>=0);
+    extend(n);
+    return inv_fact_[n];
+  }
+ 
+  static MINT nCk(const int n,const int k){
+    if(k<0||n<k)return T::raw(0);
+    extend(n);
+    return fact_[n] * inv_fact_[k] * inv_fact_[n - k];
+  }
+};
---
+++ mod/modint.cpp
@@ -0,0 +1,51 @@
+template<typename T,T MOD=998244353>
+struct Mint{
+  inline static constexpr T mod = MOD;
+  T v;
+  Mint():v(0){}
+  Mint(signed v):v(v){}
+  Mint(long long t){v=t%MOD;if(v<0)v+=MOD;}
+
+  Mint pow(long long k){
+    Mint res(1),tmp(v);
+    while(k){
+      if(k&1)res*=tmp;
+      tmp*=tmp;
+      k>>=1;
+    }
+    return res;
+  }
+
+  static Mint add_identity(){return Mint(0);}
+  static Mint mul_identity(){return Mint(1);}
+
+  Mint inv(){return pow(MOD-2);}
+
+  Mint& operator+=(Mint a){v+=a.v;if(v>=MOD)v-=MOD;return *this;}
+  Mint& operator-=(Mint a){v+=MOD-a.v;if(v>=MOD)v-=MOD;return *this;}
+  Mint& operator*=(Mint a){v=1LL*v*a.v%MOD;return *this;}
+  Mint& operator/=(Mint a){return (*this)*=a.inv();}
+
+  Mint operator+(Mint a) const{return Mint(v)+=a;}
+  Mint operator-(Mint a) const{return Mint(v)-=a;}
+  Mint operator*(Mint a) const{return Mint(v)*=a;}
+  Mint operator/(Mint a) const{return Mint(v)/=a;}
+
+  Mint operator+() const{return *this;}
+  Mint operator-() const{return v?Mint(MOD-v):Mint(v);}
+
+  bool operator==(const Mint a)const{return v==a.v;}
+  bool operator!=(const Mint a)const{return v!=a.v;}
+
+  static Mint comb(long long n,int k){
+    Mint num(1),dom(1);
+    for(int i=0;i<k;i++){
+      num*=Mint(n-i);
+      dom*=Mint(i+1);
+    }
+    return num/dom;
+  }
+
+  friend ostream& operator<<(ostream&os,const Mint &m){os<<m.v;return os;}
+  friend istream& operator>>(istream&is,Mint &m){is>>m.v;m.v%=MOD;if(m.v<0)m.v+=MOD;return is;}
+};
