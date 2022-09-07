1a9c215628ef06d92ca9c8919c6e44953439f545
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2022-09-07 13:16:06 +0900

    PointAddRangeSum

---
+++ test/library-checker/DataStructure/PointAddRangeSum.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "segtree/segtree.cpp"
+#include "algebra/group_add.cpp"
+
+using ll=long long;
+using G=Group_Add<ll>;
+
+int main(){
+  int n,q;cin>>n>>q;
+  vector<int> v(n);
+  for(int i=0;i<n;i++)cin>>v[i];
+  SegmentTree<G> seg(v);
+  while(q--){
+    int t,l,r;cin>>t>>l>>r;
+    if(t)cout<<seg.prod(l,r)<<"\n";
+    else seg.multiply(l,r);
+  }
+}
