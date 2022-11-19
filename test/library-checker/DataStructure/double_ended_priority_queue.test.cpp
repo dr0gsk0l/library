7c26a88b7b0c12306edd8d57ac94315cc1d4f1ba
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2022-11-19 19:31:01 +0900

    double_ended_pq

---
+++ test/library-checker/DataStructure/double_ended_priority_queue.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/double_ended_priority_queue"
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "superstd/multiset.cpp"
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n,q;cin>>n>>q;
+  Multiset<int> s;
+  s.scan(n);
+  while(q--){
+    int t;cin>>t;
+    if(t==0){
+      int x;cin>>x;
+      s.insert(x);
+    }
+    if(t==1){
+      int a=s.pick_mn();
+      cout<<a<<"\n";
+    }
+    if(t==2){
+      int a=s.pick_mx();
+      cout<<a<<"\n";
+    }
+  }
+}
