5b07d5b9cd9cafdf328112d61e25b17e0e929a39
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2022-11-29 06:36:46 +0900

    a

---
+++ algebra/monoid/Set.cpp
@@ -0,0 +1,9 @@
+#pragma once
+template<typename X>
+struct Monoid_Set{
+  using O=optional<X>;
+  using value_type=O;
+  static constexpr O op(const O &x,const O &y)noexcept{ return (x.has_value()?x:y); }
+  static constexpr O unit()noexcept{ return nullopt; }
+  static constexpr bool commute=false;
+};
