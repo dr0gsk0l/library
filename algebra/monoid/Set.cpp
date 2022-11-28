0cab04988a32990fcdd75dace41d9a3edea0b34a
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2022-11-29 06:44:56 +0900

    a

--- algebra/monoid/Set.cpp
+++ algebra/monoid/Set.cpp
@@ -1,6 +1,6 @@
 #pragma once
 template<typename X>
-struct Set{
+struct MonoidSet{
   using O=optional<X>;
   using value_type=O;
   static constexpr O op(const O &x,const O &y)noexcept{ return (x.has_value()?x:y); }
