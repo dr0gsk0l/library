7818d8a088070ab3b5c9ea7bdf8559bff31fd09a
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2022-12-05 18:06:16 +0900

    hoge

--- algebra/Reverse.cpp
+++ algebra/Reverse.cpp
@@ -3,5 +3,6 @@ template<typename Algebra>
 struct AlgebraReverse:Algebra{
   using X=typename Algebra::value_type;
   static constexpr X op(const X& x, const X& y){ return Algebra::op(y,x); }
-  static constexpr void chop(X&x,const X&y){ x=op(y,x); }
+  static constexpr void Rchop(X&x,const X&y){ Algebra::Lchop(y,x); }
+  static constexpr void Lchop(const X&x,X&y){ Algebra::Rchop(y,x); }
 };
