26e5ac12c78dcc9fdb30399aceced6eeff725148
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2022-12-05 17:45:47 +0900

    hoge

--- algebra/group/README.md
+++ algebra/group/README.md
@@ -3,8 +3,8 @@ template<typename X>
 struct GroupHoge{
   using value_type=X // Affine など X じゃ無い時もある
   static constexpr X op(const X& x, const X& y)
-  static constexpr void Rchop(X&x, const X&y) // x←xy
-  static constexpr void Lchop(const X&x, X&y) // y←xy
+  static constexpr void Rchop(X&x, const X&y){ x=op(x,y); }
+  static constexpr void Lchop(const X&x, X&y){ y=op(x,y); }
   static constexpr X power(const X& x, const long long n)
   static constexpr X unit()
   static constexpr X inverse(const X&x)
--- algebra/monoid/Min.cpp
+++ algebra/monoid/Min.cpp
@@ -2,8 +2,8 @@ template<typename X>
 struct MonoidMin{
   using value_type = X;
   static constexpr X op(const X &x, const X &y) noexcept { return min(x,y); }
-  static constexpr void Rchop(X&x, const X&y){ if(x<y)x=y; }
-  static constexpr void Lchop(const X&x, X&y){ if(y<x)y=x; }
+  static constexpr void Rchop(X&x, const X&y){ if(x>y)x=y; }
+  static constexpr void Lchop(const X&x, X&y){ if(y>x)y=x; }
   static constexpr X unit() { return numeric_limits<X>::max()/2; }
   static constexpr bool commute = true;
 };
