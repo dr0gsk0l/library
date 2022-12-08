3cf641d3cbdc9f6f27186b2a0f805562c559f7c8
dr0gsk0l <44800800+dr0gsk0l@users.noreply.github.com>
2022-12-08 11:13:41 +0900

    Update CumulativeMonoid.cpp

--- datastructure/CumulativeMonoid.cpp
+++ datastructure/CumulativeMonoid.cpp
@@ -3,10 +3,11 @@ class CumulativeMonoid{
   using T=typename Monoid::value_type;
   vector<T> pre,suf;
 public:
-  CumulativeMonoid():pre(1,Monoid::unit()),sum(pre){}
-  CumulativeMonoid(const vector<T>&v):pre(v.size()+1,Monoid::unit()),sum(pre){
+  CumulativeMonoid():pre(1,Monoid::unit()),suf(pre){}
+  CumulativeMonoid(const vector<T>&v):pre(v.size()+1,Monoid::unit()),suf(pre){
     for(int i=0;i<v.size();i++)pre[i+1]=Monoid::op(pre[i],v[i]);
     for(int i=v.size()-1;i>=0;i--)suf[i]=Monoid::op(v[i],suf[i+1]);
+    assert(pre.back()==suf[0]);
   }
   //[0,r)
   T pre_sum(int r){ return pre[r]; }
