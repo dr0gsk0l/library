868faeb7b809792c7e4c662b141ec1a69e4e3927
dr0gsk0l <44800800+dr0gsk0l@users.noreply.github.com>
2022-12-12 19:10:26 +0900

    Update RangeKthSmallest.test.cpp

--- test/library-checker/DataStructure/RangeKthSmallest.test.cpp
+++ test/library-checker/DataStructure/RangeKthSmallest.test.cpp
@@ -11,7 +11,7 @@ int main(){
   int n,q;cin>>n>>q;
   vector<int> v(n);
   for(int i=0;i<n;i++)cin>>v[i];
-  WaveletMatrix<int,30> WM(v);
+  WaveletMatrix<int> WM(v);
   while(q--){
     int l,r,k;cin>>l>>r>>k;
     cout<< WM.kth_smallest(l,r,k) <<"\n";
