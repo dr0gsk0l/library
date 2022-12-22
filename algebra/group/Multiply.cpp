e943edcfe06583e6dc83aa795e03d2ac7fcd80ea
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2022-12-22 12:06:10 +0900

    hoge

--- algebra/group/Multiply.cpp
+++ algebra/group/Multiply.cpp
@@ -1,5 +1,5 @@
-template<typename X>
-struct GroupMultiply {
+template<typename X,bool COMMUTE=true>
+struct GroupMultiply{
   using value_type = X;
   static constexpr X op(const X &x, const X &y) noexcept { return x * y; }
   static constexpr void Rchop(X&x, const X&y){ x*=y; }
@@ -7,5 +7,5 @@ struct GroupMultiply {
   static constexpr X inverse(const X &x) noexcept { return X(1)/x; }
   static constexpr X power(const X &x, long long n) noexcept { return x.pow(n); }
   static constexpr X unit() { return X(1); }
-  static constexpr bool commute = true;
+  static constexpr bool commute = COMMUTE;
 };
