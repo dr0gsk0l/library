dbc4ebde4e98c0dd38701efada268182a613f003
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2022-12-25 10:50:39 +0900

    hoge

--- math/F2.cpp
+++ math/F2.cpp
@@ -3,21 +3,29 @@ template<typename T,int LOG>
 class F2{
   bool is_sorted;
   vector<T> B;
-  void xor_chmin(T&a,T b){ a=min(a,a^b); }
+  bool xor_chmin(T&a,T b){ 
+    if(a>(a^b)){
+      a^=b;
+      return true;
+    }
+    return false;
+  }
 public:
   F2():is_sorted(true){}
   F2(const vector<T>&v):is_sorted(true){
     for(const T&a:v)add(a);
   }
 
-  void add(T a){
-    if(B.size()==LOG)return;
+  bool add(T a){
+    if(B.size()==LOG)return false;
     for(T e:B)xor_chmin(a,e);
     if(a){
       for(T&e:B)xor_chmin(e,a);
       if(B.size() and B.back()>a)is_sorted=false;
       B.push_back(a);
+      return true;
     }
+    return false;
   }
 
   vector<T> base(){
@@ -43,5 +51,18 @@ public:
   T kth_largest(T k){
     assert( !(k>>(B.size())) );
     return kth_smallest((T{1}<<B.size()) - k - 1);
+  }
+
+  bool in(T a){
+    if(B.size()==LOG)return true;
+    for(T e:B)xor_chmin(a,e);
+    return a;
+  }
+
+  optional<vector<T>> make(T a){
+    vector<T> v;
+    for(T e:B)if(xor_chmin(a,e))v.push_back(e);
+    if(a)return nullopt;
+    return v;
   }
 };
